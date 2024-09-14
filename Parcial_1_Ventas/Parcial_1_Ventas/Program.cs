586449065a63a0e44a628708bb76cfc6f50142e5
Vanesa Acevedo Grajales <nesa6423@gmail.com>
2024-09-13 21:12:25 -0500

    Calculos

--- Parcial_1_Ventas/Parcial_1_Ventas/Program.cs
+++ Parcial_1_Ventas/Parcial_1_Ventas/Program.cs
@@ -6,10 +6,11 @@ public class Program
     {
         // Declaración variables
 
-        decimal sueldoBase;
+       const decimal sueldoBase = 80000; // 
+        const decimal porcentajeComi = 0.10m; // Porcentaje 10% de comisión por cada venta
+       
 
-        decimal comision;
-
+        // Solicitud de datos por pantalla
         Console.Write("Ingrese el valor de la venta1: ");
         decimal venta1 = Convert.ToDecimal(Console.ReadLine());
 
@@ -18,6 +19,25 @@ public class Program
 
         Console.Write("Ingrese el valor de la venta3: ");
         decimal venta3 = Convert.ToDecimal(Console.ReadLine());
+
+        // Calculo de cada comisión por venta
+        decimal comision1 = venta1 * porcentajeComi;
+        decimal comision2 = venta2 * porcentajeComi;
+        decimal comision3 = venta3 * porcentajeComi;
+
+        // Calculo del total de las comisiones
+        decimal TotalComision = comision1 + comision2 + comision3;
+
+        // Total de dinero recibido en el mes
+        decimal devengado = sueldoBase + TotalComision;
+
+        // Promedio de comisiones
+        decimal comisiones = (TotalComision / 3);
+
+
+
+
+
 
 
     }

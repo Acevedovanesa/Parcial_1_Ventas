2600166ca878cfc4d169740331b9546c33e278cc
Vanesa Acevedo Grajales <nesa6423@gmail.com>
2024-09-13 20:37:30 -0500

    Inicialización variables

---
+++ Parcial_1_Ventas/Parcial_1_Ventas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        // Declaración variables
+
+        decimal sueldoBase;
+
+        decimal comision;
+
+        Console.Write("Ingrese el valor de la venta1: ");
+        decimal venta1 = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Ingrese el valor de la venta2: ");
+        decimal venta2 = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Ingrese el valor de la venta3: ");
+        decimal venta3 = Convert.ToDecimal(Console.ReadLine());
+
+
+    }
+}

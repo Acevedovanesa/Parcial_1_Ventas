4c91133a37728abc94a781c0ec38f7dcc6ead37a
Vanesa Acevedo Grajales <nesa6423@gmail.com>
2024-09-13 22:40:59 -0500

    Solicitud de datos

---
+++ Punto2/Punto2/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        // Solicitar al usuario que ingrese los coeficientes
+
+        Console.Write("Ingrese el coeficiente a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el coeficiente b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        
+    }
+}
+    
